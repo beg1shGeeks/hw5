0dc3c8f8c094738d47327911e3b0d41d695f3cc4
beg1shgeeks <bega1ymgeeks@gmail.com>
2023-03-20 19:14:34 +0600

    add

--- Begaiym_hw1_27-02.py
+++ Begaiym_hw1_27-02.py
@@ -1,150 +1,18 @@
-from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
-from decouple import config
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-import random
+import logging
+from config import dp
 
-TOKEN = config("TOKEN")
-
-bot = Bot(TOKEN)
-dp = Dispatcher(bot=bot)
+from handlers import client, callback,  admin, extra
 
 
-@dp.message_handler(commands=['start'])
-async def start_command(message: types.Message):
-    await message.answer("Hello world!")
+client.register_handlers_client(dp)
+callback.register_handlers_callback(dp)
+extra.register_handlers_extra(dp)
 
-
-@dp.message_handler(commands=['help'])
-async def help_command(message: types.Message):
-    await message.answer("Сам разбирайся!")
-
-
-@dp.message_handler(commands=['quiz'])
-async def quiz_1(message: types.Message):
-    markup = InlineKeyboardMarkup()
-    button_1 = InlineKeyboardButton("NEXT", callback_data="button_1")
-    markup.add(button_1)
-
-    question = "By whom invented Python?"
-    answer = [
-        "Harry Potter",
-        "Putin",
-        "Guido Van Rossum",
-        "Voldemort",
-        "Griffin",
-        "Linus Torvalds",
-    ]
-    await bot.send_poll(
-        chat_id=message.from_user.id,
-        question=question,
-        options=answer,
-        is_anonymous=False,
-        type='quiz',
-        correct_option_id=2,
-        explanation="Стыдно не знать",
-        open_period=5,
-        reply_markup=markup
-    )
-
-
-@dp.callback_query_handler(text="button_1")
-async def quiz_2(call: types.CallbackQuery):
-    markup = InlineKeyboardMarkup()
-    button_2 = InlineKeyboardButton("NEXT", callback_data="button_2")
-    markup.add(button_2)
-
-    question = "Сколько яблок на березе??"
-    answer = [
-        "12",
-        "3",
-        "БЕССКОНЕЧНОСТЬ",
-        "0",
-        "-10",
-        "999",
-    ]
-    await bot.send_poll(
-        chat_id=call.from_user.id,
-        question=question,
-        options=answer,
-        is_anonymous=False,
-        type='quiz',
-        correct_option_id=2,
-        explanation="Стыдно не знать",
-        open_period=5,
-        reply_markup=markup
-    )
-
-
-@dp.callback_query_handler(text="button_2")
-async def quiz_2(call: types.CallbackQuery):
-    markup = InlineKeyboardMarkup()
-    button_3 = InlineKeyboardButton("NEXT", callback_data="button_3")
-    markup.add(button_3)
-    question = "Сколько дней в году?"
-    answer = [
-        "12",
-        "3",
-        "БЕССКОНЕЧНОСТЬ",
-        "0",
-        "-10",
-        "365",
-    ]
-    await bot.send_poll(
-        chat_id=call.from_user.id,
-        question=question,
-        options=answer,
-        is_anonymous=False,
-        type='quiz',
-        correct_option_id=5,
-        explanation="Стыдно не знать",
-        open_period=5,
-        reply_markup=markup
-    )
-
-
-@dp.callback_query_handler(text="button_3")
-async def quiz_2(call: types.CallbackQuery):
-    question = "Есть ли у Мирлана совесть?"
-    answer = [
-        "нет",
-        "оба",
-        "да",
-        "жок",
-        "yes",
-        "no",
-    ]
-    await bot.send_poll(
-        chat_id=call.from_user.id,
-        question=question,
-        options=answer,
-        is_anonymous=False,
-        type='quiz',
-        correct_option_id=2,
-        explanation="Стыдно не знать",
-        open_period=5,
-    )
-
-
-@dp.message_handler(commands=['mem'])
-async def mem_command(message: types.Message):
-    photos = (
-        'media/img.png',
-        'media/img_1.png',
-    )
-    photo = open(random.choice(photos), 'rb')
-    await bot.send_photo(message.from_user.id, photo=photo)
-
-
-@dp.message_handler()
-async def echo(message: types.Message):
-    if message.text == "python":
-        await message.answer("I love it!")
-    elif message.text.isdigit():
-        await bot.send_message(message.from_user.id, int(message.text) ** 2)
-    else:
-        await bot.send_message(message.from_user.id, message.text)
+admin.register_handlers_client(dp)
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     executor.start_polling(dp, skip_updates=True)
+
---
+++ keyboards/client_kb.py
@@ -0,0 +1,15 @@
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+submit_markup = ReplyKeyboardMarkup(
+    resize_keyboard=True,
+    one_time_keyboard=True,
+).add(
+    KeyboardButton('да'),
+    KeyboardButton('нет')
+)
+cancel_button = KeyboardButton('Cancel')
+cancel_markup = ReplyKeyboardMarkup(
+    resize_keyboard=True,
+    one_time_keyboard=True,
+).add(
+    cancel_button
+)
